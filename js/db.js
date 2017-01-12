b2c33b21652e05565fd5db12116982dd6d1d6fcb
ram <ram.seetharaman@gmail.com>
2017-01-12 23:21:13 +0530

    Added Customer
    Added Customer Screen

--- js/db.js
+++ js/db.js
@@ -4,6 +4,7 @@ function PopulateDatabase(tx)
 {
 tx.executeSql("Create Table IF NOT EXISTS products(p_id INTEGER PRIMARY KEY AUTOINCREMENT,code integer,desc text,unit text,price REAL,status Numeric)");
 tx.executeSql("Create Table IF NOT EXISTS unit(p_id INTEGER PRIMARY KEY AUTOINCREMENT,unit text,description text,status Numeric)");
+tx.executeSql("Create Table IF NOT EXISTS customer_supplier(p_id INTEGER PRIMARY KEY AUTOINCREMENT,name text,address text,mobile_no text,email_id text,status Numeric)");
 //tx.executeSql("Insert into product values(1,'P1','Produt1',2,12.5)");
 //tx.executeSql("Insert into product values(2,'P2','Produt2',4,'12.5')");
 }
@@ -13,7 +14,7 @@ function errorDB(error)
 alert("Error on Database Creation: "+ error.message);
 }
 
-function successDB(error)
+function successDB(error)	
 {
 //alert("Databse created succ");
 }
@@ -62,6 +63,28 @@ function unit_edit(form,id)
     });
 	}); 
 }
+
+function cust_edit(form,id)
+{
+	console.log("#####"+form+"#####"+id);
+ 	database.transaction(function(tx) {
+    tx.executeSql('SELECT * FROM customer_supplier where p_id=?', [id], function(tx, result) {
+    $.each(result.rows,function(index){
+    var row = result.rows.item(index);
+	document.getElementById("p_cust_id").value=row['p_id'];
+	document.getElementById("cust_name").value=row['name'];
+	document.getElementById("address").value=row['address'];
+	document.getElementById("mobile").value=row['mobile_no'];
+	document.getElementById("email").value=row['email_id'];
+	console.log(row);
+	$.mobile.navigate( "#addcustomer" );
+	});
+	}, function(tx, error) {
+      console.log('SELECT error: ' + error.message);
+    });
+	}); 
+}
+
 
 function delete_prod(p_id)
 {
@@ -126,6 +149,33 @@ database.transaction(function(tx) {
   }); 
 }
 
+function cust_add_data(obj)
+{
+console.log(obj); 
+
+database.transaction(function(tx) {
+	if(obj.p_cust_id === undefined ||  obj.p_cust_id === null ||  obj.p_cust_id === '')
+    tx.executeSql('INSERT INTO customer_supplier(name,address,mobile_no,email_id,status) VALUES (?,?,?,?,1)', [obj.cust_name,obj.address,obj.mobile,obj.email]);
+	else
+		console.log('Transaction update ');
+    tx.executeSql('update customer_supplier set name=?, address=?, mobile_no=?, email_id=? where p_id=?', [obj.cust_name,obj.address,obj.mobile,obj.email,obj.p_cust_id]);
+  }, function(error) {
+    console.log('Transaction ERROR: ' + error.message);
+	$("#status_msg").html('<span id="error_message" class="error">Transaction ERROR:<b>' + error.message+'</b></span>');
+  }, function() {
+	  $(".status_msg").fadeIn().html('<span id="success_message" class="success"><b>Data saved successfully</b></span>');
+				setTimeout(function() {
+					$('.status_msg').fadeOut("slow");
+				}, 2000 );
+	 $('#add_cust_form').trigger("reset");
+	 $('form#add_cust_form input[type=hidden]').val('');
+	 if(obj.p_cust_id !== undefined &&  obj.p_cust_id !== null &&  obj.p_cust_id !== '')
+	 $.mobile.navigate( "#customer" );
+
+  }); 
+}
+
+
 function Prod_select()
 {
 	//console.log("kkk");
@@ -162,3 +212,21 @@ function unit_select()
     });
   });
 }	
+
+function cust_select()
+{
+//	console.log("kkk");
+	database.transaction(function(tx) {
+    tx.executeSql('SELECT * FROM customer_supplier where status = 1', [], function(tx, result) {
+		$('ul#cust_list_view').html('<li data-role="list-divider">Customer</li>');
+        $.each(result.rows,function(index){
+            var row = result.rows.item(index);
+            $('ul#cust_list_view').append('<li><a href="#" id="'+row['p_id']+'" onclick="cust_edit(\'customer\','+row['p_id']+');"><h3 class="ui-li-heading">'+row['name']+'</h3><p class="ui-li-desc">Address: '+row['address']+'      '+'Mobile No: '+row['mobile_no']+'      '+'Email Id: '+row['email_id']+'</p></a></li>');
+        });
+ 
+        $('ul#cust_list_view').listview('refresh');
+    }, function(tx, error) {
+      console.log('SELECT error: ' + error.message);
+    });
+  });
+}				
--- js/index.js
+++ js/index.js
@@ -27,6 +27,12 @@ switch(page)
 	case "product":
 	$.mobile.navigate( "#product" );
 	break;
+	case "frmunits":
+	$.mobile.navigate( "#frmunits" );
+	break;
+	case "customer":
+	$.mobile.navigate( "#customer" );
+	break;
 }	
 }
 
@@ -40,6 +46,12 @@ function save_unit(obj)
 {
 	console.log(obj);
 	unit_add_data(obj); 
+}	
+
+function save_cust(obj)
+{
+	console.log(obj);
+	cust_add_data(obj); 
 }	
 
 $( document ).ready(function() {
@@ -63,6 +75,17 @@ $("#add_unit_form").validate({
 	submitHandler: function (form) {
 		var $this = $(form);		
 		save_unit($this.serializeFormJSON());
+        return false;	
+	}	  
+});
+
+$("#add_cust_form").validate({
+		errorPlacement: function (error, element) {
+        error.appendTo(element.parent().prev());
+    },
+	submitHandler: function (form) {
+		var $this = $(form);		
+		save_cust($this.serializeFormJSON());
         return false;	
 	}	  
 });
@@ -161,6 +184,54 @@ $("#frmunits").on("pageshow" , function() {
   
 });
 
+$("#customer").on("pageshow" , function() {
+  cust_select();
+    $( "#customer" ).on( "swipeleft", "#prod_list_view li a", function( event ) {
+        var listitem = $( this ),
+            // These are the classnames used for the CSS transition
+            dir = event.type === "swipeleft" ? "left" : "",
+            // Check if the browser supports the transform (3D) CSS transition
+            transition = $.support.cssTransform3d ? dir : false;
+			
+			//console.log(this.id);
+            confirmAndDelete( listitem, transition ,this.id );
+    });
+    // If it's not a touch device...
+    if ( ! $.mobile.support.touch ) {
+        // Remove the class that is used to hide the delete button on touch devices
+        $( "#prod_list_view" ).removeClass( "touch" );
+        // Click delete split-button to remove list item
+        $( ".delete" ).on( "click", function() {
+            var listitem = $( this ).parent( "li" );
+            confirmAndDelete( listitem );
+        });
+    }
+    function confirmAndDelete( listitem, transition ,p_id ) {
+        // Highlight the list item that will be removed
+        listitem.children( ".ui-btn" ).addClass( "ui-btn-active" );
+        // Inject topic in confirmation popup after removing any previous injected topics
+        $( "#confirm .topic" ).remove();
+        listitem.find( ".topic" ).clone().insertAfter( "#question" );
+        // Show the confirmation popup
+        $( "#confirm" ).popup( "open" );
+        // Proceed when the user confirms
+        $( "#confirm #yes" ).on( "click", function() {
+			listitem.remove();
+			//listitem.removeClass( "ui-btn-active" );
+			$( "#confirm #yes" ).off();
+			delete_prod(p_id);		
+			listitem.refresh();
+        });
+        // Remove active state and unbind when the cancel button is clicked
+        $( "#confirm #cancel" ).on( "click", function() {
+            listitem.removeClass( "ui-btn-active" );
+            $( "#confirm #yes" ).off();
+        });
+    }
+  
+});
+
+
 function BTlist()
 {
 	BTPrinter.list(function(data){
@@ -248,6 +319,21 @@ $(document).on('submit', '#add_unit_form', function (e) {
 	
 }); 
 
+
+$(document).on('submit', '#add_cust_form', function (e) {
+	
+	
+    //cache the form element for use in this function
+    var $this = $(this);
+
+    //prevent the default submission of the form
+    e.preventDefault();
+
+    //run an AJAX post request to your server-side script, $this.serialize() is the data from your form being added to the request
+	
+	console.log($this.serialize());
+	
+}); 
 
 function print_text(txt)
 {
